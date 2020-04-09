f2bb55a870f9f277eac415d9cfb73df618d4428d
WashiraSuyajai <washira.suyajai@gmail.com>
2020-04-09 22:07:34 +0700

    Certificates content added

--- react-app/src/components/certificates.js
+++ react-app/src/components/certificates.js
@@ -1,22 +1,47 @@
 import React, { Component } from "react";
-import htmlCert from '../components/certificate-pics/html-fundamental.JPG'
+import styled from "styled-components";
+import htmlCert from "../components/certificate-pics/html-fundamental.JPG";
+import pythonCert from "../components/certificate-pics/python-for-data-science.jpg";
+import bigdataCert from "../components/certificate-pics/data-bigdata.JPG";
+import reactCert from "../components/certificate-pics/react-at-futureskill.jpg";
+import technicalCert from "../components/certificate-pics/technical-support-fundamentals.jpg";
 
 class Certificates extends Component {
   render() {
+    const Image = styled.img`
+      margin-top: 20px;
+      margin-left: 2.5vw;
+      margin-right: 2.5vw;
+      margin-bottom: 20px;
+      width: 300px;
+      height: auto;
+      border: 1px solid red;
+    `;
     return (
-    <div>
-      This is My Certificates
-      <div className='row'>
-        <div className='col'>
-          The computing languages
+      <div className="container">
+        <h1 style={{ textAlign: "center", marginTop: "5vw" }}>
+          My Certificates
+        </h1>
+
+        <h3 style={{ marginTop: "5vw" }}>The Programming Languages</h3>
+        <div className="row">
+          <Image classNane="" src={htmlCert} />
+          <Image classNane="" src={pythonCert} />
         </div>
-        <div className='col'>
-          <img src={htmlCert}/>
-          <br/>
-          <img/>
+        <h3 style={{ marginTop: "2.5vw" }}>The Web Application Skill</h3>
+        <div className="row">
+          <Image classNane="" src={reactCert} />
+        </div>
+        <h3 style={{ marginTop: "2.5vw" }}>Data Analytic and Bigdata</h3>
+        <div className="row">
+          <Image classNane="" src={bigdataCert} />
+        </div>
+        <h3 style={{ marginTop: "2.5vw" }}>Other Course I've learned</h3>
+        <div className="row">
+          <Image classNane="" src={technicalCert} />
         </div>
       </div>
-      </div>)
+    );
   }
 }
 
