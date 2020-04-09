5ac7bdb96756edf9be5d2192574f321b2161cd1a
WashiraSuyajai <washira.suyajai@gmail.com>
2020-04-09 22:07:09 +0700

    Minor change

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -9,123 +9,108 @@ import Certificates from "./components/certificates";
 import Contact from "./components/contact";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-
 class App extends Component {
   state = {
     isOpen: false,
+    navbarButton: false,
   };
 
   toggle = () => this.setState({ isOpen: !this.state.isOpen });
+  toggleNav = () => this.setState({ isOpen: !this.state.navbarButton });
   render() {
     const menu = `dropdown-menu${this.state.isOpen ? " show" : ""}`;
     return (
-      <div className="App">
-        {/* Navbar all icons */}
-        {/* 1. web icon */}
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <a className="navbar-brand" href="/">
-            Washira
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
-          {/* 2. This is menu */}
-          {/* 2.1 Nornal menu */}
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item active">
-                <a className="nav-link" href="/">
-                  Home <span className="sr-only">(current)</span>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/blogs">
-                  Blogs
-                </a>
-              </li>
-              {/* 2.2 This is drop down menu */}
-              <li className="nav-item dropdown" onClick={this.toggle}>
-                <div>
-                  <a
-                    className="nav-link dropdown-toggle"
-                    type="button"
-                    id="navbarDropdownMenuLink"
-                    role="button"
-                    data-toggle="dropdown"
-                    aria-haspopup="true"
-                    aria-expanded="false"
-                  >
-                    About
-                  </a>
-
-                  <div
-                    className={menu}
-                    aria-labelledby="navbarDropdownMenuLink"
-                  >
-                    <a className="dropdown-item" href="/profile">
-                      Profile
-                    </a>
-                    <a className="dropdown-item" href="/certificates">
-                      Certificates
-                    </a>
-                    <div className="dropdown-divider"></div>
-                    <a className="dropdown-item" href="/contact">
-                     Contact
-                    </a>
+      <div>
+        <div className="App container-fluid">
+          {/* Navbar all icons */}
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container">
+              {/* 1. web icon */}
+              <a className="navbar-brand" href="/">
+                Washira
+              </a>
+              {/* 2. This is menu */}
+              {/* 2.1 Nornal menu */}
+              <div className="navbar">
+                <ul className="navbar-nav mr-auto">
+                  <div className="row">
+                    <div className="col">
+                      <li className="nav-item active">
+                        <a className="nav-link text-uppercase" href="/about">
+                          About <span className="sr-only">(current)</span>
+                        </a>
+                      </li>
+                    </div>
+                    <div className="col">
+                      <li className="nav-item">
+                        <a className="nav-link text-uppercasse" href="/blogs">
+                          Blogs
+                        </a>
+                      </li>
+                    </div>
+                    {/* 2.2 This is drop down menu */}
+                    <div className="col">
+                      <li className="nav-item dropdown" onClick={this.toggle}>
+                        <div>
+                          <a
+                            className="nav-link dropdown-toggle text-uppercase"
+                            type="button"
+                            id="navbarDropdownMenuLink"
+                            role="button"
+                            data-toggle="dropdown"
+                            aria-haspopup="true"
+                            aria-expanded="false"
+                          >
+                            My Story
+                          </a>
+                          <div
+                            className={menu}
+                            aria-labelledby="navbarDropdownMenuLink"
+                          >
+                            <a className="dropdown-item" href="/profile">
+                              Profile
+                            </a>
+                            <a className="dropdown-item" href="/certificates">
+                              Certificates
+                            </a>
+                            <div className="dropdown-divider"></div>
+                            <a className="dropdown-item" href="/contact">
+                              Contact
+                            </a>
+                          </div>
+                        </div>
+                      </li>
+                    </div>
+                    {/* 3. This si disable menu */}
+                    <div className="col">
+                      <li className="nav-item">
+                        <a
+                          className="nav-link disabled"
+                          href="#"
+                          tabIndex="-1"
+                          aria-disabled="true"
+                        >
+                          Disabled
+                        </a>
+                      </li>
+                    </div>
                   </div>
-                </div>
-              </li>
-
-              {/* 3. This si disable menu */}
-              <li className="nav-item">
-                <a
-                  className="nav-link disabled"
-                  href="#"
-                  tabIndex="-1"
-                  aria-disabled="true"
-                >
-                  Disabled
-                </a>
-              </li>
-            </ul>
-
-            {/* 4. This is searcher */}
-            <form className="form-inline my-2 my-lg-0">
-              <input
-                className="form-control mr-sm-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button
-                className="btn btn-outline-success my-2 my-sm-0"
-                type="submit"
-              >
-                Search
-              </button>
-            </form>
-          </div>
-        </nav>
-        {/* Navbar all icons ended */}
-        {/*Thanks:
+                </ul>
+              </div>
+            </div>
+          </nav>
+          {/* Navbar all icons ended */}
+          {/*Thanks:
         https://stackoverflow.com/questions/50980046/bootstrap-dropdown-not-working-in-react/54188034 */}
 
-        {/* Route */}
-        <Route path="/blogs" component={Blogs} />
-        <Route path="/about" component={About} />
-        <Route path="/profile" component={Profile} />
-        <Route path="/certificates" component={Certificates} />
-        <Route path="/contact" component={Contact} />
-        <Footer />
+          {/* Route */}
+          <Route path="/blogs" component={Blogs} />
+          <Route path="/about" component={About} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/certificates" component={Certificates} />
+          <Route path="/contact" component={Contact} />
+          <Footer />
+        </div>
       </div>
     );
   }
