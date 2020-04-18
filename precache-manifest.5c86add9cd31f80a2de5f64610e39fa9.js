c31cf41f44976ffaf0304a4b3a5f0aee10b05227
WashiraSuyajai <washira.suyajai@gmail.com>
2020-04-18 21:23:40 +0700

    Updates

---
+++ precache-manifest.5c86add9cd31f80a2de5f64610e39fa9.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50b26ffe497dce062c43e22780623cd2",
+    "url": "/profile-in-react/index.html"
+  },
+  {
+    "revision": "48f2f2f3b25ba83d96da",
+    "url": "/profile-in-react/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "4e1f2e62a148b8f716b6",
+    "url": "/profile-in-react/static/css/main.47b1bab8.chunk.css"
+  },
+  {
+    "revision": "48f2f2f3b25ba83d96da",
+    "url": "/profile-in-react/static/js/2.82cc088f.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/profile-in-react/static/js/2.82cc088f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e1f2e62a148b8f716b6",
+    "url": "/profile-in-react/static/js/main.175fec2e.chunk.js"
+  },
+  {
+    "revision": "defd36548f9aff123d8b",
+    "url": "/profile-in-react/static/js/runtime-main.1a7f7a87.js"
+  },
+  {
+    "revision": "9d33de97a095dbcae4bf299396cfeff0",
+    "url": "/profile-in-react/static/media/data-bigdata.9d33de97.JPG"
+  },
+  {
+    "revision": "e5ffb626fa7bd477a0081f17ba2af25b",
+    "url": "/profile-in-react/static/media/html-fundamental.e5ffb626.JPG"
+  },
+  {
+    "revision": "5c570427ee23f69853d28aec805eee79",
+    "url": "/profile-in-react/static/media/instagram.5c570427.png"
+  },
+  {
+    "revision": "b55e3ce6949517e6fe9fa94f00a12586",
+    "url": "/profile-in-react/static/media/line.b55e3ce6.png"
+  },
+  {
+    "revision": "b9e8e7a12fb2d36c07326018fe6de840",
+    "url": "/profile-in-react/static/media/pink-sea.b9e8e7a1.jpg"
+  },
+  {
+    "revision": "546dccf04666526ef57cb2cef7b311ab",
+    "url": "/profile-in-react/static/media/profile.546dccf0.JPG"
+  },
+  {
+    "revision": "df9a162a151a8c312754ca57e722bc70",
+    "url": "/profile-in-react/static/media/python-for-data-science.df9a162a.jpg"
+  },
+  {
+    "revision": "127943bc7a1da1345ef1a24ee68edbf5",
+    "url": "/profile-in-react/static/media/react-at-futureskill.127943bc.jpg"
+  },
+  {
+    "revision": "23a81f9308841ccbe0a8b7ef87c5566d",
+    "url": "/profile-in-react/static/media/technical-support-fundamentals.23a81f93.jpg"
+  }
+]);
