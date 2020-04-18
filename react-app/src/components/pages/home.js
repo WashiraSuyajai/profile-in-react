f5f2a36474a13492bbc21ce91d3f19ecb85ed94f
WashiraSuyajai <washira.suyajai@gmail.com>
2020-04-18 16:54:59 +0700

    Filled a cover image to page

--- react-app/src/components/pages/home.js
+++ react-app/src/components/pages/home.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import styled from "styled-components";
+import { Image } from "react-bootstrap";
+import PinkSea from "./pink-sea.jpg";
 
 const P = styled.p`
   margin-top: 2vw;
@@ -13,30 +15,35 @@ const PBottom = styled(P)`
 class Home extends Component {
   render() {
     return (
-      <div className="container">
-        <h1
-          style={{
-            marginTop: "5vw",
-            fontFamily: '"Trebuchet MS", Helvetica, sans-serif',
-          }}
-        >
-          Welcome to my page
-        </h1>
-        <P>
-          ยินดีต้อนรับทุกท่านเข้าสู่ Web Page ของผมนะครับ ในเวปไซต์นี้
-          ผมได้จัดทำขึ้นจากการทดลองฝึกเขียน Web Site ด้วย React
-          และมีการตกแต่งหน้าตาด้วย CSS Framework อย่าง React Bootstrap และ
-          React-Semantic-UI ซึ่งอาจจะสวย (รึเปล่าหว่า?)
-        </P>
-        <P>
-          นอกเหนือจากการโชว์ประวัติส่วนตัวและความเป็นตัวเองผ่านเวปไซต์นี้แล้ว
-          ผมยังมีแผนที่จะทำเวปนี้ให้เป็นที่เผยแพร่บทความต่างๆที่ผมจะเขียนในอนาคต
-          (ซึ่งเป็นเหตุผลว่า ทำไมถึงมีเมนู Blogs ที่เมนูบาร์){" "}
-        </P>
-        <PBottom>
-          ดังนั้น ในอนาคตไม่ใกล้ไม่ไกลนี้
-          ทุกคนจะได้พบกับมัน(บทความ)อย่างแน่นอนครับ ดีใจที่ได้พบกับทุกท่านครับ
-        </PBottom>
+      <div style={{ marginBottom: "5vw" }}>
+        <div>
+          <Image src={PinkSea} style={{ width: "100vw", height: "400px" }} />
+        </div>
+        <div className="container">
+          <h1
+            style={{
+              marginTop: "5vw",
+              fontFamily: '"Trebuchet MS", Helvetica, sans-serif',
+            }}
+          >
+            Welcome to my page
+          </h1>
+          <P>
+            ยินดีต้อนรับทุกท่านเข้าสู่ Web Page ของผมนะครับ ในเวปไซต์นี้
+            ผมได้จัดทำขึ้นจากการทดลองฝึกเขียน Web Site ด้วย React
+            และมีการตกแต่งหน้าตาด้วย CSS Framework อย่าง React Bootstrap และ
+            React-Semantic-UI ซึ่งอาจจะสวย (รึเปล่าหว่า?)
+          </P>
+          <P>
+            นอกเหนือจากการโชว์ประวัติส่วนตัวและความเป็นตัวเองผ่านเวปไซต์นี้แล้ว
+            ผมยังมีแผนที่จะทำเวปนี้ให้เป็นที่เผยแพร่บทความต่างๆที่ผมจะเขียนในอนาคต
+            (ซึ่งเป็นเหตุผลว่า ทำไมถึงมีเมนู Blogs ที่เมนูบาร์){" "}
+          </P>
+          <PBottom>
+            ดังนั้น ในอนาคตไม่ใกล้ไม่ไกลนี้
+            ทุกคนจะได้พบกับมัน(บทความ)อย่างแน่นอนครับ ดีใจที่ได้พบกับทุกท่านครับ
+          </PBottom>
+        </div>
       </div>
     );
   }
