0942ca2166bfc7ff6a63197cf31cad9d8dcc831d
WashiraSuyajai <washira.suyajai@gmail.com>
2020-04-19 17:52:10 +0700

    Use HashRouter

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,14 +2,17 @@ import React, { Component } from "react";
 import "./App.css";
 import NavBar from "./components/navbar";
 import Footer from "./components/footer";
+import { HashRouter } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <NavBar />
-        <Footer />
-      </div>
+      <HashRouter basename="/">
+        <div className="App">
+          <NavBar />
+          <Footer />
+        </div>
+      </HashRouter>
     );
   }
 }
