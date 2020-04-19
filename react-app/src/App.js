35539ff4920d67161ac6e08461be025225fcdafe
WashiraSuyajai <washira.suyajai@gmail.com>
2020-04-19 21:36:03 +0700

    A little change

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import NavBar from "./components/navbar";
 import Footer from "./components/footer";
-import { HashRouter } from "react-router-dom";
+
 
 class App extends Component {
   render() {
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -4,14 +4,14 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 
 
 ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <Router basename={process.env.PUBLIC_URL}>
     <App />
-  </BrowserRouter>,
+  </Router>,
   document.getElementById("root")
 );
 
