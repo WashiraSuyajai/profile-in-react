2d9ec72b8a668eb757226f1f001783c4b9529906
WashiraSuyajai <washira.suyajai@gmail.com>
2020-04-26 22:27:39 +0700

    Filled cover pic

--- react-app/src/components/pages/profile.js
+++ react-app/src/components/pages/profile.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import styled from "styled-components";
 import { Grid, Menu, Segment, Item } from "semantic-ui-react";
 import SearchBar from "./videos/searchbar";
 import { Switch, Route } from "react-router-dom";
@@ -9,6 +8,8 @@ import History from "./my-info/history";
 import Performances from "./my-info/performances";
 import Experiences from "./my-info/experiences";
 import Link from "./my-info/link";
+import { Image } from "react-bootstrap";
+import Sake from "./cover-pics/sake.jpg";
 
 class Profile extends Component {
   state = { activeItem: "Bio" };
@@ -31,60 +32,65 @@ class Profile extends Component {
     //ถ้า activeItem เป็นค่าอะไร ให้แสดง content ของค่านั้น
     // content สามารถเป็น JSX ได้
     return (
-      <div
-        className="ui container"
-        style={{ marginTop: "5vw", marginBottom: "5vw" }}
-      >
-        <h1 style={{ fontFamily: "chivo" }}>My Info</h1>
-        <Grid
-          columns={3}
-          divided
-          relaxed
-          stackable
-          style={{ marginTop: "2vw" }}
+      <div>
+        <div>
+          <Image src={Sake} style={{ width: "100vw", height: "400px" }} />
+        </div>
+        <div
+          className="ui container"
+          style={{ marginTop: "5vw", marginBottom: "5vw" }}
         >
-          <Grid.Column>
-            <Item>
-              <Item.Image size="massive" src={ProfilePic} />
-            </Item>
-          </Grid.Column>
-          <Grid.Column width={3}>
-            <Menu fluid vertical tabular>
-              <Menu.Item
-                name="Bio"
-                active={activeItem === "Bio"}
-                onClick={this.handleItemClick}
-              />
-              <Menu.Item
-                name="History"
-                active={activeItem === "History"}
-                onClick={this.handleItemClick}
-              />
-              <Menu.Item
-                name="Experiences"
-                active={activeItem === "Experiences"}
-                onClick={this.handleItemClick}
-              />
-              <Menu.Item
-                name="Performances"
-                active={activeItem === "Performances"}
-                onClick={this.handleItemClick}
-              />
-              <Menu.Item
-                name="Link"
-                active={activeItem === "Link"}
-                onClick={this.handleItemClick}
-              />
-            </Menu>
-          </Grid.Column>
-
-          <Grid.Column stretched width={7}>
-            <Segment content={content} />
-          </Grid.Column>
-        </Grid>
-        <Switch>
-          <Route exact path="/searchbar" component={SearchBar} />
-        </Switch>
+          <h1 style={{ fontFamily: "chivo" }}>My Info</h1>
+          <Grid
+            columns={3}
+            divided
+            relaxed
+            stackable
+            style={{ marginTop: "2vw" }}
+          >
+            <Grid.Column>
+              <Item>
+                <Item.Image size="massive" src={ProfilePic} />
+              </Item>
+            </Grid.Column>
+            <Grid.Column width={3}>
+              <Menu fluid vertical tabular>
+                <Menu.Item
+                  name="Bio"
+                  active={activeItem === "Bio"}
+                  onClick={this.handleItemClick}
+                />
+                <Menu.Item
+                  name="History"
+                  active={activeItem === "History"}
+                  onClick={this.handleItemClick}
+                />
+                <Menu.Item
+                  name="Experiences"
+                  active={activeItem === "Experiences"}
+                  onClick={this.handleItemClick}
+                />
+                <Menu.Item
+                  name="Performances"
+                  active={activeItem === "Performances"}
+                  onClick={this.handleItemClick}
+                />
+                <Menu.Item
+                  name="Link"
+                  active={activeItem === "Link"}
+                  onClick={this.handleItemClick}
+                />
+              </Menu>
+            </Grid.Column>
+            {/* Details */}
+            <Grid.Column stretched width={7}>
+              <Segment content={content} />
+            </Grid.Column>
+          </Grid>
+          <Switch>
+            <Route exact path="/searchbar" component={SearchBar} />
+          </Switch>
+        </div>
       </div>
     );
   }
--- react-app/src/components/pages/project.js
+++ react-app/src/components/pages/project.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
+import { Image } from "react-bootstrap";
+import Basket from "./cover-pics/basket.jpg";
 
 class Project extends Component {
   render() {
     return (
-      <div className="container">
-        <h1>Project</h1>
-        <p> เป็นหน้าที่แสดงโปรเจคต่างๆที่ผมเคยฝึกทำเอาไว้ ทั้งตอนที่เรียนจาก Online Course และจากที่ฝึกทำด้วยตัวเองครับ</p>
+      <div>
+        <div>
+          <Image src={Basket} style={{ width: "100vw", height: "400px" }} />
+        </div>
+        <div className="container">
+          <h1>Project</h1>
+          <p>
+            {" "}
+            เป็นหน้าที่แสดงโปรเจคต่างๆที่ผมเคยฝึกทำเอาไว้ ทั้งตอนที่เรียนจาก
+            Online Course และจากที่ฝึกทำด้วยตัวเองครับ
+          </p>
+        </div>
       </div>
     );
   }
