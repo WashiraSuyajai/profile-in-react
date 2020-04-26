bde744edffeabc5fde13b3bac5c5c672a5346612
WashiraSuyajai <washira.suyajai@gmail.com>
2020-04-26 22:27:06 +0700

    Filled dark mode

--- react-app/src/components/footer.js
+++ react-app/src/components/footer.js
@@ -9,62 +9,70 @@ const image = {
   marginLeft: "10px",
 };
 
-class Footer extends Component {
-  render() {
-    return (
-      <div
-        className="container-fluid"
-        style={{
-          color: "black",
-          backgroundColor: "#FDD3FB",
-          paddingTop: "10px",
-        }}
-      >
-        <div>
-          <div className="" style={{ textAlign: "center" }}>
-            <h4>washira</h4>
-          </div>
-          <h5 id="Follow" style={{ marginLeft: "60vw" }}>
-            Follow me
-          </h5>
-          {/* Icons */}
-          <div
-            className="row"
-            style={{
-              alignItems: "normal",
-              marginLeft: "58vw",
-              marginTop: "15px",
-              marginBottom: "30px",
-            }}
-          >
-            {/* Facebook */}
-            <div>
-              <a href="https://www.facebook.com/washira.suyajai">
-                <img src={facebookIcon} style={image} />
-              </a>
-            </div>
+const light = {
+  color: "black",
+  backgroundColor: "#FDD3FB",
+  paddingTop: "10px",
+  transition: "all 0.25s linear",
+};
 
-            {/* Line */}
-            <div>
-              <a href="">
-                <img src={lineIcon} style={image} />
-              </a>
-            </div>
+const dark = {
+  color: "white",
+  backgroundColor: "#17202A",
+  paddingTop: "10px",
+  transition: "all 0.25s linear",
+};
 
-            {/* Instagram */}
-            <div>
-              <a href="#">
-                <img src={instagramIcon} style={image} />
-              </a>
-            </div>
+function Footer({ footerTheme }) {
+  return (
+    <div
+      className="container-fluid"
+      style={footerTheme === "light" ? light : dark}
+    >
+      <div>
+        <div className="" style={{ textAlign: "center" }}>
+          <h4>washira</h4>
+        </div>
+        <h5 id="Follow" style={{ marginLeft: "60vw" }}>
+          Follow me
+        </h5>
+        {/* Icons */}
+        <div
+          className="row"
+          style={{
+            alignItems: "normal",
+            marginLeft: "58vw",
+            marginTop: "15px",
+            marginBottom: "30px",
+          }}
+        >
+          {/* Facebook */}
+          <div>
+            <a href="https://www.facebook.com/washira.suyajai">
+              <img src={facebookIcon} style={image} />
+            </a>
           </div>
-          <div style={{ marginLeft: "60vw", marginBottom: "20px" }}>
-            2020@washira
+
+          {/* Line */}
+          <div>
+            <a href="">
+              <img src={lineIcon} style={image} />
+            </a>
+          </div>
+
+          {/* Instagram */}
+          <div>
+            <a href="#">
+              <img src={instagramIcon} style={image} />
+            </a>
           </div>
         </div>
+        <div style={{ marginLeft: "60vw", marginBottom: "20px" }}>
+          2020@washira
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Footer;
